21d55c5dff5840b942efd042d6d499c3d6f7ed88
MarniSaiSanjay <marnisaisanjay@gmail.com>
2022-11-15 14:25:15 +0530

    picture changed

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // const dburl =  process.env.DB_URL || 'mongodb://localhost:27017/DBMS1';
-const dburl = process.env.DB_URL || 'mongodb://localhost:27017/DBMS1';
+const dburl =  'mongodb://localhost:27017/DBMS1';
 
 mongoose.connect(dburl, {
     useNewUrlParser: true,
